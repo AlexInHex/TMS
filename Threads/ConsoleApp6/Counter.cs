7976ccbfc47bc41b7d5d4ebfe11c23e045c503cf
Alexey Vlasenko <88191198+AlexInHex@users.noreply.github.com>
2021-10-15 13:10:58 +0300

    Add files via upload

---
+++ Threads/ConsoleApp6/Counter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Counter
+    {
+        private readonly int[] array;
+        private long totalCount;
+        
+        public Counter(int[] array)
+        {
+            this.array = array;
+        }
+
+        private void InternalCalculate(object obj)
+        {
+            int[] partOfArray = obj as int[];
+            foreach (var item in partOfArray)
+            {              
+                Interlocked.Add(ref totalCount, item);
+            }
+            
+        }
+
+        public long Calculate()
+        {
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                int[] partArray = array.Skip(i * 100_000).Take(100_000).ToArray();
+
+                threads[i] = new Thread(InternalCalculate);
+                threads[i].Start(partArray);                
+            }
+            foreach (var item in threads)
+            {
+                item.Join();
+            }
+
+            return totalCount;
+
+
+        }
+    }
+}
---
+++ Threads/ConsoleApp6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ticker ticker = new Ticker(() => Console.WriteLine("///"), 1);
+            //ticker.Start();
+            //Thread.Sleep(5000);
+            //ticker.Stop();
+
+            int[] array = Enumerable.Range(1, 1000_000).ToArray();
+            long count = 0;
+
+            foreach (var item in array)
+            {
+                count += item;
+            }
+            Console.WriteLine(count);
+            Counter counter = new Counter(array);
+            Console.WriteLine(counter.Calculate());
+
+            Console.ReadKey();
+        }
+    }
+}
